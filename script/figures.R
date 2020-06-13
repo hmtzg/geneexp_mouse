fc906c170fd76d7a8944f93906980035446d452b
melike <donertas.melike@gmail.com>
2020-06-13 12:00:51 +0100

    figures updated

--- script/figures.R
+++ script/figures.R
@@ -6,7 +6,7 @@ theme_set(theme_pubr(base_size = 6, legend = 'top') +
 pntnorm <- (1/0.352777778)
 tissuecol = setNames(c("#233789", "#f49e92", "#801008","#dbb32e"),c('Cortex','Lung','Liver','Muscle'))
 varcol = setNames(c('dodgerblue','firebrick3'),c('div','con'))
-regcol = setNames(c('#52233B','#234361'),c('up','down'))
+regcol = setNames(c('rosybrown3','paleturquoise3'),c('up','down'))
 
 sample_info = readRDS('./data/processed/figures/tidy/sample_info.rds')
 expr = readRDS('./data/processed/figures/tidy/expression.rds')
@@ -134,7 +134,7 @@ all_raw_pc2age = pca_dat %>%
 
 all_raw_pc12 = ggarrange(all_raw_pca12, ggarrange(all_raw_pc1age, all_raw_pc2age, ncol = 1, nrow= 2), ncol = 2, nrow= 1, widths = c(1,0.5), common.legend = F,labels = c('a',NA), align = 'v')
 
-ggsave('./results/pca_all_raw12_wage.pdf',all_raw_pc12, units = 'cm', width = 16, height = 6, useDingbats = T)
+ggsave('./results/pca_all_raw12_wage.pdf',all_raw_pc12, units = 'cm', width = 16, height = 6, useDingbats = F)
 ggsave('./results/pca_all_raw12_wage.png',all_raw_pc12, units = 'cm', width = 16, height = 6)
 
 all_raw_pca34 = pca_dat %>%
@@ -189,7 +189,7 @@ all_raw_pc4age = pca_dat %>%
 
 all_raw_pc34 = ggarrange(all_raw_pca34, ggarrange(all_raw_pc3age, all_raw_pc4age, ncol = 1, nrow= 2), ncol = 2, nrow= 1, widths = c(1,0.5), legend = F, labels = c('a',NA))
 
-ggsave('./results/pca_all_raw34_wage.pdf', all_raw_pc34, units = 'cm', width = 16, height = 6, useDingbats = T)
+ggsave('./results/pca_all_raw34_wage.pdf', all_raw_pc34, units = 'cm', width = 16, height = 6, useDingbats = F)
 ggsave('./results/pca_all_raw34_wage.png', all_raw_pc34, units = 'cm', width = 16, height = 6)
 
 all_raw_pc34_2 = ggarrange(all_raw_pca34, ggarrange(all_raw_pc3age, all_raw_pc4age, ncol = 1, nrow= 2), ncol = 2, nrow= 1, widths = c(1,0.5), legend = F, labels = c('b',NA), align = 'v')
@@ -213,7 +213,7 @@ all_raw_pca12_nt = pca_dat %>%
   ylab(paste('PC2 (', round(all_notissue_var[2]*100),'%)',sep='')) +
   guides(color = guide_legend('Tissue'), 
          size = guide_legend('Age')) +
-  theme(legend.position = 'top',
+  theme(legend.position = c(0.7,0.9),
         legend.direction = 'horizontal',
         legend.box = 'vertical',
         legend.background = element_rect(fill = 'gray85',color = 'gray25')) 
@@ -221,7 +221,7 @@ all_raw_pca12_nt = pca_dat %>%
 ggsave('./results/pca_all_nt12.pdf',all_raw_pca12_nt, units = 'cm', width = 8, height = 8, useDingbats =F)
 ggsave('./results/pca_all_nt12.png',all_raw_pca12_nt, units = 'cm', width = 8, height = 8)
 
-all_raw_pc1age = pca_dat %>%
+all_raw_pc1age_nt = pca_dat %>%
   select(-varExp) %>%
   filter(period == 'all', type == 'notissue') %>%
   spread(key = 'PC',value = 'value') %>%
@@ -235,9 +235,9 @@ all_raw_pc1age = pca_dat %>%
   guides(color = F) +
   xlab('Age (log2)')
 
-all_raw_pc2age = pca_dat %>%
+all_raw_pc2age_nt = pca_dat %>%
   select(-varExp) %>%
-  filter(period == 'all', type == 'raw') %>%
+  filter(period == 'all', type == 'notissue') %>%
   spread(key = 'PC',value = 'value') %>%
   left_join(sample_info) %>%
   ggplot(aes(x = age, y = PC2, color = tissue)) +
@@ -249,13 +249,12 @@ all_raw_pc2age = pca_dat %>%
   guides(color = F) +
   xlab('Age (log2)')
 
-all_raw_pc12 = ggarrange(all_raw_pca12, ggarrange(all_raw_pc1age, all_raw_pc2age, ncol = 1, nrow= 2), ncol = 2, nrow= 1, widths = c(1,0.5), common.legend = F,labels = c('a',NA), align = 'v')
+all_raw_pc12_nt = ggarrange(all_raw_pca12_nt, ggarrange(all_raw_pc1age_nt, all_raw_pc2age_nt, ncol = 1, nrow= 2), ncol = 2, nrow= 1, widths = c(1,0.5), common.legend = F,labels = c('a',NA), align = 'v')
 
-ggsave('./results/pca_all_raw12_wage.pdf',all_raw_pc12, units = 'cm', width = 16, height = 6, useDingbats = T)
-ggsave('./results/pca_all_raw12_wage.png',all_raw_pc12, units = 'cm', width = 16, height = 6)
+ggsave('./results/pca_all_nt12_wage.pdf',all_raw_pc12_nt, units = 'cm', width = 16, height = 9, useDingbats = F)
+ggsave('./results/pca_all_nt12_wage.png',all_raw_pc12_nt, units = 'cm', width = 16, height = 9)
 
-
-expr_ch %>%
+reversalgene_most = expr_ch %>%
   select(-p,-FDR) %>%
   spread(key = period, value =`Expression Change`) %>%
   mutate(rev = aging *development) %>%
@@ -263,13 +262,19 @@ expr_ch %>%
   left_join(expr) %>%
   inner_join(sample_info) %>%
   ggplot(aes(x =age, y= expression, color = tissue)) +
+  geom_vline(xintercept = 90, linetype = 'dashed', color = 'gray35') +
   geom_smooth() +
   geom_point() +
-  scale_x_continuous(trans = 'log2') +
-  scale_color_manual(values = tissuecol)
+  scale_x_continuous(trans = 'log2', breaks = c(2,10,30,90,300, 900)) +
+  scale_color_manual(values = tissuecol) +
+  xlab('Age (log2)') +
+  ylab('Gene Expression') +
+  guides(color = F)
 
+ggsave('./results/reversalgene.pdf',reversalgene_most, units = 'cm', width = 8, height = 6, useDingbats = F)
+ggsave('./results/reversalgene.png',reversalgene_most, units = 'cm', width = 8, height = 6)
 
-expr_ch %>%
+allgenes_exp_ch_p = expr_ch %>%
   group_by(tissue,period) %>%
   summarise( up = sum(`Expression Change`>0,na.rm=T),
              down = sum(`Expression Change`<0,na.rm=T)) %>%
@@ -279,9 +284,17 @@ expr_ch %>%
   facet_wrap(~tissue, ncol=4) +
   geom_bar(stat= 'identity', position ='fill') +
   scale_fill_manual(values = regcol) +
-  geom_hline(yintercept = 0.5)
+  geom_hline(yintercept = 0.5, linetype = 'dashed', color = 'gray35') +
+  xlab(NULL) +ylab(NULL) +
+  guides(fill = guide_legend('Direction of Expression Change', 
+                             override.aes = list(size = 2))) +
+  theme(legend.position = 'top',
+        legend.background = element_rect(fill = 'gray85',color = 'gray25')) 
 
-expr_ch %>%
+ggsave('./results/allgenes_exp_ch.pdf',allgenes_exp_ch_p, units = 'cm', width = 10, height = 6, useDingbats = F)
+ggsave('./results/allgenes_exp_ch.png',allgenes_exp_ch_p, units = 'cm', width = 10, height = 6)
+
+siggenes_exp_ch_p = expr_ch %>%
   filter(FDR<=0.1) %>%
   group_by(tissue,period) %>%
   summarise( up = sum(`Expression Change`>0,na.rm=T),
@@ -294,46 +307,83 @@ expr_ch %>%
   geom_bar(stat= 'identity', position = 'dodge') +
   scale_fill_manual(values = regcol, drop=FALSE) +
   scale_y_continuous(trans = 'log10') +
-  geom_text(aes(label = n), color='white', position = position_dodge(width = 1), angle=90, hjust = 1, vjust = 0.5)
+  geom_text(aes(label = n), color='gray15', position = position_dodge(width = 1), angle=90, hjust = 1, vjust = 0.5, size = 6/pntnorm)+
+  guides(fill = guide_legend('Direction of Expression Change', 
+                             override.aes = list(size = 2))) +
+  theme(legend.position = 'bottom',
+        legend.background = element_rect(fill = 'gray85',color = 'gray25')) +
+  xlab(NULL)  + ylab(NULL) +
+  ggtitle('Number of Genes with a Significant Change (FDR≤0.1)')
 
-cov_ch %>%
+ggsave('./results/siggenes_exp_ch.pdf',siggenes_exp_ch_p, units = 'cm', width = 10, height = 6, useDingbats = F)
+ggsave('./results/siggenes_exp_ch.png',siggenes_exp_ch_p, units = 'cm', width = 10, height = 6)
+
+top_divcon_gene_dat = cov_ch %>%
   select(-p,-FDR) %>%
   spread(key = period, value =`CoV_change`) %>%
   mutate(rev = aging *development) %>%
-  top_n(n=10,wt=-rev) %>%
+  top_n(n=1,wt=-(rev)) %>%
   left_join(expr) %>%
-  inner_join(sample_info) %>%
-  ggplot(aes(x =age, y= expression, color = tissue)) +
-  geom_smooth(se=F) +
-  # geom_point() +
-  geom_vline(xintercept = 90)+
+  inner_join(sample_info) 
+top_divcon_gene = ggplot(top_divcon_gene_dat, aes(x =age, y= expression, color = tissue)) +
+  geom_smooth(se=F,size = 0.7) +
+  geom_point(size=0.5) +
+  geom_vline(xintercept = 90, linetype = 'dashed', color = 'gray35')+
   scale_x_continuous(trans = 'log2') +
   scale_color_manual(values = tissuecol) +
-  facet_wrap(~gene_id, scales = 'free')
+  ggtitle(unique(top_divcon_gene_dat$gene_id)) +
+  guides(color = guide_legend('Tissue', 
+                             override.aes = list(size = 2))) +
+  theme(legend.position = 'top',
+        legend.background = element_rect(fill = 'gray85',color = 'gray25')) +
+  xlab('Age (log2)') + ylab('Gene Expression')
 
+ggsave('./results/top_divcon_gene.pdf',top_divcon_gene, units = 'cm', width = 8, height = 8, useDingbats = F)
+ggsave('./results/top_divcon_gene.png',top_divcon_gene, units = 'cm', width = 8, height = 8)
 
-cov_dat %>%
+top_divcon_gene_cov = top_divcon_gene_dat %>%
+  group_by(gene_id, ind_id, age) %>%
+  summarise(sd = sd(expression),
+            mean = mean(expression),
+            cov = sd/mean) %>%
+  ggplot(aes(x = age, y= cov)) +
+  geom_smooth(size = 0.7) +
+  geom_point(size=0.5) +
+  geom_vline(xintercept = 90, linetype = 'dashed', color = 'gray35')+
+  scale_x_continuous(trans = 'log2') +
+  ggtitle(unique(top_divcon_gene_dat$gene_id)) +
+  xlab('Age (log2)') + ylab('CoV')
+
+ggsave('./results/top_divcon_gene_cov.pdf',top_divcon_gene_cov, units = 'cm', width = 8, height = 8, useDingbats = F)
+ggsave('./results/top_divcon_gene_cov.png',top_divcon_gene_cov, units = 'cm', width = 8, height = 8)
+
+top_divcon_gene_p = ggarrange(top_divcon_gene+ggtitle(NULL),top_divcon_gene_cov+ggtitle(NULL),labels = 'auto',ncol=2,nrow=1, align = 'hv')
+ggsave('./results/top_divcon_gene_expcov.pdf',top_divcon_gene_p, units = 'cm', width = 10, height = 5, useDingbats = F)
+ggsave('./results/top_divcon_gene_expcov.png',top_divcon_gene_p, units = 'cm', width = 10, height = 5)
+
+cov_dat_sum = cov_dat %>%
   mutate(ind_id = factor(ind_id)) %>%
   left_join(unique(select(sample_info,-tissue,-sample_id))) %>%
   group_by(ind_id, age) %>%
-  summarise(meanCoV = mean(CoV),
-            medCoV = median(CoV),
-            q3CoV = quantile(CoV,0.75),
-            q1CoV = quantile(CoV,0.25)) %>%
-  ggplot(aes(x = age, y= meanCoV)) +
-  # geom_pointrange(aes(ymin = q1CoV, ymax = q3CoV)) +
+  summarise(meanCoV = mean(CoV)) %>% 
+  mutate(period = c('aging','development')[1+(age<=90)])
+
+cov_cordat = group_by(ungroup(cov_dat_sum),period) %>%
+  summarise(cor = cor.test(meanCoV,age,method = 's')$est,
+            cor.p = cor.test(meanCoV,age,method = 's')$p.val)
+
+cov_mean = ggplot(cov_dat_sum, aes(x = age, y= meanCoV)) +
   geom_point() +
-  geom_smooth(se=F) +
+  geom_smooth(se=T,color = 'midnightblue') +
   scale_x_continuous(trans = 'log2') +
-  geom_vline(xintercept = 90)
+  geom_vline(xintercept = 90, linetype='dashed',color = 'gray35') +
+  xlab('Age (log2)') + ylab('Mean CoV') +
+  annotate('text',x=95,y=0.5075,label='Aging',hjust=0,size = 8/pntnorm)+
+  annotate('text',x = 95, y=0.505, label = parse(text = paste('rho["CoV,age"] ==' ,(round(filter(cov_cordat,period=='aging')$cor,3)))),hjust=0,size = 8/pntnorm) +
+  annotate('text',x = 95, y=0.5025, label = parse(text = paste0('p ==' ,(round(filter(cov_cordat,period=='aging')$cor.p,2)))),hjust=0,size = 8/pntnorm)+
+  annotate('text',x=8,y=0.4825,label='Development',hjust=0,size = 8/pntnorm)+
+  annotate('text',x = 8, y=0.48, label = parse(text = paste('rho["CoV,age"] ==' ,(round(filter(cov_cordat,period=='development')$cor,3)))),hjust=0,size = 8/pntnorm) +
+  annotate('text',x = 8, y=0.4775, label = parse(text = paste0('p ==' ,(round(filter(cov_cordat,period=='development')$cor.p,2)))),hjust=0,size = 8/pntnorm)
 
-cov_gsea %>%
-  filter(p.adj <= 0.1) %>%
-  select(Description) %>%
-  left_join(cov_gsea) %>%
-  select(Description, NES, p.adj, period) %>%
-  head()
-  ggplot(aes(x = period, y= NES, fill = p.adj<=0.1)) +
-  geom_boxplot()
-
-
+ggsave('./results/cov_mean.pdf',cov_mean, units = 'cm', width = 8, height = 8, useDingbats = F)
+ggsave('./results/cov_mean.png',cov_mean, units = 'cm', width = 8, height = 8)
